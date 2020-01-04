53d8793953ac22695d72bd2a2557969186e82498
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-05 01:18:25 +0300

    IRepository add

---
+++ WM.BL/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WM.BL
+{
+    public interface IRepository <T>
+    {
+        T RetriveById(int id);
+        List<T> RetrieveAll();
+        void Add(T entity);
+
+    }
+}
