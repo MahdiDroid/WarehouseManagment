cb2f989f789c1cdd0ae530a3379eb70d42e19a00
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-04 21:56:44 +0300

    initial commit

---
+++ WM.BL/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WM.BL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+
+        public Product Retrieve()
+        {
+            return new Product(); 
+        }
+        public List<Product> RetriveAll()
+        {
+            var products = new List<Product>();
+            return products;
+        }
+    }
+
+}
